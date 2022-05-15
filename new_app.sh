fc7b06318457b6a84e604c44c46390e511e9b31b
Murilo Romeo <muriloh65@gmail.com>
2022-05-14 21:25:42 -0300

    changed to new_app.sh

--- new_app.sh
+++ new_app.sh
@@ -5,6 +5,7 @@ grep -lR "????" env-example | xargs sed -i "s/????/$name/g"
 mkdir ../$name
 cp -a ../"$(basename $PWD)"/. ../$name
 cd ../$name
+rm -rf ./new_app.sh
 rm -rf ../"$(basename $PWD)"
 rm -rf .git
 git init
