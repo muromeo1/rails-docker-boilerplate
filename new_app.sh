3370504810af1ea505e0a6d58f832cd9b07a784c
Murilo Romeo <muriloh65@gmail.com>
2022-05-14 21:34:12 -0300

    fixed removing old boilerplate project

--- new_app.sh
+++ new_app.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 read -p 'Enter new project name: ' name
-grep -lR "????" env-example | xargs sed -i "s/????/$name/g"
+grep -lR "????" ./env-example | xargs sed -i "s/????/$name/g"
 mkdir ../$name
 cp -a ../"$(basename $PWD)"/. ../$name
 cd ../$name
 rm -rf ./new_app.sh
-rm -rf ../"$(basename $PWD)"
+sudo rm -rf ../"$(basename $PWD)"
 rm -rf .git
 git init
